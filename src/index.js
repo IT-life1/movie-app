9f43371a44230adfcfa81f2a94dacf949f6d5b40
IT-life1 <sanya.mozgov@inbox.ru>
2021-08-23 19:12:00 +0300

    git commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Bulb from './Bulb';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Bulb />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
